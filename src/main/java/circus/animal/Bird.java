dbd8db00b4b3bfd8aea1fb5dea1ae36cd3fe5e3d
BenjaminChan <42601850+benchan911@users.noreply.github.com>
2020-02-12 11:22:06 +0800

    Update Bird.java
    Adding speak function "Tweet Tweet"

--- src/main/java/circus/animal/Bird.java
+++ src/main/java/circus/animal/Bird.java
@@ -4,4 +4,7 @@ public abstract class Bird extends Animal {
     public void fly() {
         System.out.println("Whee ...");
     }
+    public String speak(){
+        return "tweet tweet";
+    }
 }
